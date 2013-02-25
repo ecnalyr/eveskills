208466a0f744a6de3798c784484560dc4fb97a59
Jason Rylance <ecnalyr@gmail.com>
2013-02-25 11:06:56 -0600

    Pushing to Github

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+
+  require 'open-uri'
+  require 'nokogiri'
+  
   def index
     @users = User.all
+
+    @api_results = Nokogiri.XML(open("https://api.eveonline.com/char/CharacterSheet.xml.aspx?keyID=1867200&vCode=oReWk9nG5QutSKn03RINpBXajnDtU2egla3uTr4dLQDV4kVTeQodWgy1He7ECeU4").read)
   end
 end
