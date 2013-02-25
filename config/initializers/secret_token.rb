fede7731d39470ded613a66e2fb3fd31c7ff740c
Jason Rylance <ecnalyr@gmail.com>
2013-02-25 10:09:09 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eveskills::Application.config.secret_token = 'ce293d422fdf74fbca4421376e03d2b5bc40af11ab15b7989c9c8d1deb358a0f1978da53178510ece77e92fe52ab0bea53771d3dd62bb0202f6681b20af4386d'
