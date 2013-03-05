9b9478d87a502dc476d4e8ec39777b568e028806
Jason Rylance <ecnalyr@gmail.com>
2013-03-05 11:34:32 -0600

    cleaned up home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,5 @@ class HomeController < ApplicationController
   require 'nokogiri'
   
   def index
-    @users = User.all
-
-    @api_results = Nokogiri.XML(open("https://api.eveonline.com/char/CharacterSheet.xml.aspx?keyID=1867200&vCode=oReWk9nG5QutSKn03RINpBXajnDtU2egla3uTr4dLQDV4kVTeQodWgy1He7ECeU4").read)
   end
 end
